64bf8a0e8cee0473fd886951e15f322e09d0f582
yura-prots <yuraprots@gmail.com>
2024-02-10 15:56:17 +0200

    create fetchTrending( )

--- src/api/MoviedbAPI.js
+++ src/api/MoviedbAPI.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'a23f5de8750d4e18c3afb539dab409ad';
+
+export const fetchTrending = async () => {
+  const response = await axios({
+    url: `${BASE_URL}trending/all/day?api_key=${API_KEY}`,
+    method: 'get',
+  });
+
+  return response.data;
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import { fetchTrending } from 'api/MoviedbAPI';
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const response = await fetchTrending();
+
+        setMovies(response);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <div>
+      <h2>Trending</h2>
+    </div>
+  );
 };
 
 export default Home;
